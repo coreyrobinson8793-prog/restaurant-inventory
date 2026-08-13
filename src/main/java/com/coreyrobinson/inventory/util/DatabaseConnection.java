25e90f2381696fe2f1dd7b9acdb4b0039da10119
Corey Robinson <corey.robinson8793@gmail.com>
2026-08-13 11:21:07 -0400

    Add native Windows packaging: shaded jar, launcher class, and external database config

--- src/main/java/com/coreyrobinson/inventory/util/DatabaseConnection.java
+++ src/main/java/com/coreyrobinson/inventory/util/DatabaseConnection.java
@@ -5,6 +5,8 @@
  */
 package com.coreyrobinson.inventory.util;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -24,17 +26,31 @@ public class DatabaseConnection {
 	}
 	
 	static {
-		Properties props = new Properties();
-		try (InputStream input = DatabaseConnection.class.getClassLoader().getResourceAsStream("database.properties")) {
-			props.load(input);
-			URL = props.getProperty("db.url");
-			USER = props.getProperty("db.user");
-			PASSWORD = props.getProperty("db.password");
-			DRIVER = props.getProperty("db.driver");
-			Class.forName(DRIVER);
-		} catch (IOException | ClassNotFoundException e) {
-			throw new RuntimeException("Failed to initialize database connection", e);
-		}
+	    Properties props = new Properties();
+	    try {
+	        File external = new File("database.properties");
+	        if (external.exists()) {
+	            try (InputStream input = new FileInputStream(external)) {
+	                props.load(input);
+	            }
+	        } else {
+	            try (InputStream input = DatabaseConnection.class.getClassLoader()
+	                    .getResourceAsStream("database.properties")) {
+	                if (input == null) {
+	                    throw new RuntimeException("database.properties not found. "
+	                            + "Place it next to the application executable.");
+	                }
+	                props.load(input);
+	            }
+	        }
+	        URL = props.getProperty("db.url");
+	        USER = props.getProperty("db.user");
+	        PASSWORD = props.getProperty("db.password");
+	        DRIVER = props.getProperty("db.driver");
+	        Class.forName(DRIVER);
+	    } catch (IOException | ClassNotFoundException e) {
+	        throw new RuntimeException("Failed to initialize database connection", e);
+	    }
 	}
 	
 	/**
